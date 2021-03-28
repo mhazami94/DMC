d8ded8c72ba55170b7b13b95b274dc520ada332d
Mohammad Azami <mhazami73@gmail.com>
2021-03-28 23:27:06 +0300

    added winning trader

---
+++ WebApp/Controllers/WinningTraderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    public class WinningTraderController : Controller
+    {
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [Route("/Winning-Trader")]
+        public IActionResult WinningTrader()
+        {
+            return View();
+        }
+
+        [Route("/Winning-Trader-SignUp")]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
