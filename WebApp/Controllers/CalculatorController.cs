443ffe143aa3ea7e7c63d9512d81416daf6af6fa
MohammadAzamiadarabi <mohammad.azamiadarabi@stablex.net>
2021-04-07 11:24:08 +0300

    finished calculator

--- WebApp/Controllers/CalculatorController.cs
+++ WebApp/Controllers/CalculatorController.cs
@@ -32,8 +32,8 @@ namespace WebApp.Controllers
             else ViewBag.Tips = null;
         }
 
-        [Route("/Calculaor/Result")]
-        public async Task<IActionResult> GetResult(string type, string unit, string range, string L, string K)
+        [Route("/Calculaor/GetTeminat")]
+        public async Task<IActionResult> GetTeminat(string type, string unit, string range, string L, string K)
         {
             double result = 0;
             await new FXServices().GetCalculationPrices();
@@ -55,6 +55,81 @@ namespace WebApp.Controllers
             }
             return Content($"{result}");
 
+        }
+
+        [Route("/Calculaor/GetZarar")]
+        public async Task<IActionResult> GetZarar(string type, string unit, string range, string L, string P1, string P2)
+        {
+            double result = 0;
+            await new FXServices().GetCalculationPrices();
+            var price1 = FXServices.UnitPrices.FirstOrDefault(x => x.Title == $"{unit}USD");
+            var price2 = FXServices.UnitPrices.FirstOrDefault(x => x.Title == $"USD{unit}");
+
+            if (type.ToEnum<Enums.CalculatorType>() == Enums.CalculatorType.Buy)
+            {
+                if (price1 == null)
+                    result = System.Convert.ToDouble(range) * System.Convert.ToDouble(L) * (System.Convert.ToDouble(P2) - System.Convert.ToDouble(P1)) / price2.BuyPrice;
+                else
+                    result = System.Convert.ToDouble(range) * System.Convert.ToDouble(L) * (System.Convert.ToDouble(P2) - System.Convert.ToDouble(P1)) / price1.BuyPrice;
+            }
+            else if (type.ToEnum<Enums.CalculatorType>() == Enums.CalculatorType.Sell)
+            {
+                if (price1 == null)
+                    result = System.Convert.ToDouble(range) * System.Convert.ToDouble(L) * (System.Convert.ToDouble(P1) - System.Convert.ToDouble(P2)) / price2.SalePrice;
+                else
+                    result = System.Convert.ToDouble(range) * System.Convert.ToDouble(L) * (System.Convert.ToDouble(P1) - System.Convert.ToDouble(P2)) / price1.BuyPrice;
+            }
+            return Content($"{result}");
+        }
+
+        [Route("/Calculaor/GetPip")]
+        public async Task<IActionResult> GetPip(string type, string unit, string range, string L, string Pip)
+        {
+            double result = 0;
+            await new FXServices().GetCalculationPrices();
+            var price1 = FXServices.UnitPrices.FirstOrDefault(x => x.Title == $"{unit}USD");
+            var price2 = FXServices.UnitPrices.FirstOrDefault(x => x.Title == $"USD{unit}");
+
+            if (type.ToEnum<Enums.CalculatorType>() == Enums.CalculatorType.Buy)
+            {
+                if (price1 == null)
+                    result = System.Convert.ToDouble(range) * System.Convert.ToDouble(L) * System.Convert.ToDouble(Pip) / price2.BuyPrice;
+                else
+                    result = System.Convert.ToDouble(range) * System.Convert.ToDouble(L) * System.Convert.ToDouble(Pip) / price1.BuyPrice;
+            }
+            else if (type.ToEnum<Enums.CalculatorType>() == Enums.CalculatorType.Sell)
+            {
+                if (price1 == null)
+                    result = System.Convert.ToDouble(range) * System.Convert.ToDouble(L) * System.Convert.ToDouble(Pip) / price2.SalePrice;
+                else
+                    result = System.Convert.ToDouble(range) * System.Convert.ToDouble(L) * System.Convert.ToDouble(Pip) / price1.SalePrice;
+            }
+            return Content($"{result}");
+        }
+
+        [Route("/Calculaor/GetPrice")]
+        public async Task<IActionResult> GetPrice(string type, string tip)
+        {
+            double result = 0;
+            await new FXServices().GetCalculationPrices();
+            var item = FXServices.UnitPrices.FirstOrDefault(x => x.Title == tip);
+
+            switch (type.ToEnum<Enums.CalculatorType>())
+            {
+                case Enums.CalculatorType.None:
+                    result = 0;
+                    break;
+                case Enums.CalculatorType.Buy:
+                    result = item.BuyPrice;
+                    break;
+                case Enums.CalculatorType.Sell:
+                    result = item.SalePrice;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+            return Content($"{result}");
         }
     }
 }
